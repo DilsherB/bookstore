c86ebe98367a92c8e20395fba2dbf844e959daae
Dilsher Balouch <dilsherb@gmail.com>
2023-06-08 22:27:18 +0300

    menu added.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-const Menu = () => {
+// Navbar component
+const Navbar = () => {
+  const menuItems = {
+    Books: "BOOKS",
+    Categories: "CATEGORIES",
+  };
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className="d-inline-flex px-3 w-100 bg-light">
+      <ul className="d-flex gap-5 align-items-center">
+        <h5 className="text-primary fs-2 fw-bold">Bookstore CMS</h5>
+        {Object.keys(menuItems).map((item) => (
+          <li key={item} className="list-group-item">
+            <NavLink to={`${item}`} className="text-decoration-none text-dark">
+              {menuItems[item]}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default Menu;
+export default Navbar;

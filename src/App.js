6f859cf795f2e28e9c46e6ee3cae2973e59ac8b3
Dilsher <dilsherb@gmail.com>
2023-06-08 13:55:43 +0300

    Routers setted up.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import Books from "./components/Books";
 import Categories from "./components/Categories";
 import Menu from "./components/Menu";
@@ -6,8 +7,10 @@ function App() {
   return (
     <div className="App">
       <Menu />
-      <Books />
-      <Categories />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }

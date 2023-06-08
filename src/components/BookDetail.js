f293f7c48c48247b46387903a0c2fdaacae66cac
Dilsher Balouch <dilsherb@gmail.com>
2023-06-08 15:37:56 +0300

    BookDetail function completed.

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,9 +1,37 @@
 import React from "react";
 
 const BookDetail = () => {
+  const booksDB = [
+    {
+      id: 1,
+      type: "Action",
+      bookName: "The Hunger Games",
+      author: "Suzanne Collins",
+    },
+    {
+      id: 2,
+      type: "Science Fiction",
+      bookName: "Dune",
+      author: "Frank Herbert",
+    },
+    {
+      id: 3,
+      type: "Economy",
+      bookName: "Capital in the Twenty-First Century",
+      author: "Suzanne Collins",
+    },
+  ];
   return (
     <div>
-      <h1>Book Detail</h1>
+      {booksDB.map((book) => {
+        return (
+          <div key={book.id}>
+            <h4>{book.type}</h4>
+            <h1>{book.bookName}</h1>
+            <p>{book.author}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };

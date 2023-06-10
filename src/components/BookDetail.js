096a537a160274bd815a6af44b688e29b83986aa
Dilsher <dilsherb@gmail.com>
2023-06-10 09:46:25 +0300

    bookprogress componenet deleted.

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import BookAction from "./BookAction";
-import BookProgress from "./BookProgress";
+// import BookProgress from "./BookProgress";
 
 const BookDetail = () => {
   const booksDB = [
@@ -12,6 +12,11 @@ const BookDetail = () => {
       CommentsBtn: "Comments",
       RemoveBtn: "Remove",
       EditBtn: "Edit",
+      Percentage: "64%",
+      Complete: "Complete",
+      CurrentChapter: "CURRENT CHAPTER",
+      Chapter: "Chapter 17",
+      UpdateProgress: "UPDATE PROGRESS",
     },
     {
       id: 2,
@@ -21,6 +26,11 @@ const BookDetail = () => {
       CommentsBtn: "Comments",
       RemoveBtn: "Remove",
       EditBtn: "Edit",
+      Percentage: "8%",
+      Complete: "Complete",
+      CurrentChapter: "CURRENT CHAPTER",
+      Chapter: 'Chapter 3: "A Lesson Learned"',
+      UpdateProgress: "UPDATE PROGRESS",
     },
     {
       id: 3,
@@ -30,6 +40,11 @@ const BookDetail = () => {
       CommentsBtn: "Comments",
       RemoveBtn: "Remove",
       EditBtn: "Edit",
+      Percentage: "0%",
+      Complete: "Complete",
+      CurrentChapter: "CURRENT CHAPTER",
+      Chapter: "Introduction",
+      UpdateProgress: "UPDATE PROGRESS",
     },
   ];
   return (
@@ -38,7 +53,7 @@ const BookDetail = () => {
         return (
           <>
             <div className="d-flex bg-white p-3 my-3 border rounded justify-content-between">
-              <div key={book.id} className="my-3">
+              <div key={book.id} className="my-3 w-50">
                 <p className="fw-bold text-secondary lh-1">{book.type}</p>
                 <p className="fw-bold fs-4 lh-1">{book.bookName}</p>
                 <p className="text-primary lh-1 fw-light">{book.author}</p>
@@ -53,8 +68,27 @@ const BookDetail = () => {
                   <span>{book.EditBtn}</span>
                 </p>
               </div>
-              <div>
-                <BookProgress />
+              <div className="w-50">
+                <div className="d-flex gap-5 h-100 align-items-center">
+                  <div className="d-flex h-100 align-items-center gap-3">
+                    <div className="border border-3 p-5 rounded-circle" />
+                    <div>
+                      <p>
+                        {book.Percentage}
+                        <br />
+                        {book.Complete}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="vr" />
+                  <div>
+                    <p className="text-secondary fs-6">{book.CurrentChapter}</p>
+                    <p>{book.Chapter}</p>
+                    <button type="submit" className="btn btn-primary" style={{ width: "max-content" }}>
+                      UPDATE PROGRESS
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </>

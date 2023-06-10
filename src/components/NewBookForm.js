3da4f50961390745d4ebd1678117b4bce3fb62c0
Dilsher <dilsherb@gmail.com>
2023-06-10 10:40:50 +0300

    newbookform component completed.

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -3,7 +3,30 @@ import React from "react";
 const NewBookForm = () => {
   return (
     <div>
-      <p className="fs-4 ">ADD NEW BOOK</p>
+      <p className="fs-4 text-secondary">ADD NEW BOOK</p>
+      <form>
+        <div className="d-flex justify-content-between">
+          <div className="form-group col-6 flex-row">
+            <input type="text" className="form-control" placeholder="Book title" />
+          </div>
+          <div className="form-group col-3 flex-row">
+            <option>
+              <select
+                name="category"
+                id="category"
+                className="form-select form-control"
+              >
+                <option value="Action">Action</option>
+                <option value="Science Fiction">Science Fiction</option>
+                <option value="Economy">Economy</option>
+              </select>
+            </option>
+          </div>
+          <div className="form-group col-2 col-sm-3 flex-row-3">
+            <p className="btn btn-primary form-control">ADD BOOk</p>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };

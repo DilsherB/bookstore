4f0f88553b0d5c49127c4c22d602f7feb669117c
Dilsher <dilsherb@gmail.com>
2023-06-11 10:39:23 +0300

    media query applied for small screens

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,30 +9,38 @@ const Navbar = () => {
     Categories: "CATEGORIES",
   };
   return (
-    // <div className="container bg-success w-100">
-    <ul className="container d-flex align-items-center mt-2 bg-white rounded border">
-      <h3 className="text-primary fw-bold" style={{ width: "25%" }}>
+    <ul className="container d-flex align-items-center mt-2 bg-white rounded border mediaQueryMenu">
+      <h3 className="text-primary fw-bold mediaQueryLogo" style={{ width: "25%" }}>
         Bookstore CMS
       </h3>
-      <div className="d-flex gap-3 ms-5" style={{ width: "75%" }}>
-        {Object.keys(menuItems).map((item) => (
-          <li
-            key={item}
-            className="list-group-item mx-3"
-            style={{ width: "15%" }}
-          >
-            <NavLink to={`${item}`} className="text-decoration-none text-dark">
-              {menuItems[item]}
-            </NavLink>
-          </li>
-        ))}
+      <div
+        className="d-inline-flex justify-content-between subMediaQueryMenu"
+        style={{ width: "100%" }}
+      >
+        <div className="d-flex gap-5 ms-5">
+          {Object.keys(menuItems).map((item) => (
+            <li
+              key={item}
+              className="list-group-item mx-3 mediaQueryMargin"
+              style={{ width: "15%" }}
+            >
+              <NavLink
+                to={`${item}`}
+                className="text-decoration-none text-dark"
+              >
+                {menuItems[item]}
+              </NavLink>
+            </li>
+          ))}
+        </div>
+        <div>
+          <PermIdentityIcon
+            className="text-primary border border-2 rounded-circle"
+            style={{ float: "right", cursor: "pointer" }}
+          />
+        </div>
       </div>
-      <PermIdentityIcon
-        className="text-primary border border-2 rounded-circle"
-        style={{ float: "right", cursor: "pointer" }}
-      />
     </ul>
-    // </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./bookstore.css";
+import "./mediaQueries.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 

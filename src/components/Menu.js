7810e54736a10013b96fc16ad793bdcf2f2a683c
Dilsher <dilsherb@gmail.com>
2023-06-11 08:32:04 +0300

    menu styled.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,33 +9,30 @@ const Navbar = () => {
     Categories: "CATEGORIES",
   };
   return (
-    <div className="container bg-light w-100 px-3">
-      <ul className="d-flex align-items-center pt-2">
-        <h3 className="text-primary fw-bold" style={{ width: "25%" }}>
-          Bookstore CMS
-        </h3>
-        <div className="d-flex gap-3 ms-5" style={{ width: "75%" }}>
-          {Object.keys(menuItems).map((item) => (
-            <li
-              key={item}
-              className="list-group-item mx-3"
-              style={{ width: "15%" }}
-            >
-              <NavLink
-                to={`${item}`}
-                className="text-decoration-none text-dark"
-              >
-                {menuItems[item]}
-              </NavLink>
-            </li>
-          ))}
-        </div>
-        <PermIdentityIcon
-          className="text-primary border border-2 rounded-circle"
-          style={{ float: "right", cursor: "pointer" }}
-        />
-      </ul>
-    </div>
+    // <div className="container bg-success w-100">
+    <ul className="container d-flex align-items-center mt-2 bg-white rounded border">
+      <h3 className="text-primary fw-bold" style={{ width: "25%" }}>
+        Bookstore CMS
+      </h3>
+      <div className="d-flex gap-3 ms-5" style={{ width: "75%" }}>
+        {Object.keys(menuItems).map((item) => (
+          <li
+            key={item}
+            className="list-group-item mx-3"
+            style={{ width: "15%" }}
+          >
+            <NavLink to={`${item}`} className="text-decoration-none text-dark">
+              {menuItems[item]}
+            </NavLink>
+          </li>
+        ))}
+      </div>
+      <PermIdentityIcon
+        className="text-primary border border-2 rounded-circle"
+        style={{ float: "right", cursor: "pointer" }}
+      />
+    </ul>
+    // </div>
   );
 };
 

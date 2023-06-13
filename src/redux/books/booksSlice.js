853f824aea6e0a8739a1cf903f4c8e2261b47164
Dilsher <dilsherb@gmail.com>
2023-06-13 15:15:55 +0300

    fetching data from booksSlice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,7 +2,50 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const booksSlice = createSlice({
   name: "book",
-  initialState: [],
+  initialState: [
+    {
+      item_id: "item1",
+      title: "The Great Gatsby",
+      author: "John Smith",
+      category: "Fiction",
+      CommentsBtn: "Comments",
+      RemoveBtn: "Remove",
+      EditBtn: "Edit",
+      Percentage: "64",
+      Complete: "Complete",
+      CurrentChapter: "CURRENT CHAPTER",
+      Chapter: "Chapter 17",
+      UpdateProgress: "UPDATE PROGRESS",
+    },
+    {
+      item_id: "item2",
+      title: "Anna Karenina",
+      author: "Leo Tolstoy",
+      category: "Fiction",
+      CommentsBtn: "Comments",
+      RemoveBtn: "Remove",
+      EditBtn: "Edit",
+      Percentage: "8",
+      Complete: "Complete",
+      CurrentChapter: "CURRENT CHAPTER",
+      Chapter: 'Chapter 3: "A Lesson Learned"',
+      UpdateProgress: "UPDATE PROGRESS",
+    },
+    {
+      item_id: "item3",
+      title: "The Selfish Gene",
+      author: "Richard Dawkins",
+      category: "Nonfiction",
+      CommentsBtn: "Comments",
+      RemoveBtn: "Remove",
+      EditBtn: "Edit",
+      Percentage: "0",
+      Complete: "Complete",
+      CurrentChapter: "CURRENT CHAPTER",
+      Chapter: "Introduction",
+      UpdateProgress: "UPDATE PROGRESS",
+    },
+  ],
   reducers: {
     addBook: (state, action) => {
       state.push(action.payload);

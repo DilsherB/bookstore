a42d1526b242801172ae1e466334c9c295a1994b
Dilsher <dilsherb@gmail.com>
2023-06-14 09:26:57 +0300

    addNewBook functionality added.

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addBook } from "../redux/books/booksSlice";
 
 const NewBookForm = () => {
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books);
+  const handleAddBook = (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const author = e.target[1].value;
+    const category = e.target[2].value;
+    const itemId = books.length + 1;
+    const Percentage = 0;
+    const Chapter = "still to start";
+    dispatch(
+      addBook({
+        title,
+        author,
+        category,
+        itemId,
+        Percentage,
+        Chapter,
+      })
+    );
+  };
   return (
     <div>
       <p className="fs-4 text-secondary">ADD NEW BOOK</p>
-      <form>
+      <form onSubmit={handleAddBook}>
         <div className="d-flex justify-content-between mediaQueryNewBookForm pb-5">
           <div className="form-group col-3 flex-row">
             <input
@@ -43,5 +66,4 @@ const NewBookForm = () => {
     </div>
   );
 };
-
 export default NewBookForm;
